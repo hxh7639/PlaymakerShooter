eee2b2e9855bbcc39bd470c32014b917b9fe559f
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2018-03-04 19:04:56 -0600

    3-4-18 line start - for day/night changes

---
+++ PlayMakerShooter/Assets/Andy/TimeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+    [SerializeField] private Light sun;
+    [SerializeField] private float secondsInFullDay = 120f;
+
+
+}
