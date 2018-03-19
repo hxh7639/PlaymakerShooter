30546f6ba9afc277ffdad618722bf1c1b6a8bf61
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2018-03-18 22:25:37 -0500

    3/18/18 - Sleeping part 1 - enable and disable movement.

---
+++ PlayMakerShooter/Assets/Andy/DisableManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DisableManager : MonoBehaviour {
+    [SerializeField] private vp_FPInput vp_FPInput;
+    [SerializeField] private vp_FPCamera vp_FPCamera;
+
+    public void DisablePlayer()
+    {
+        vp_FPInput.MouseCursorForced = true;
+        vp_FPCamera.enabled = false;
+
+    }
+
+    public void EnablePlayer()
+    {
+        vp_FPInput.MouseCursorForced = false;
+        vp_FPCamera.enabled = true;
+    }
+
+}
---
+++ PlayMakerShooter/Assets/Andy/SleepController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    [SerializeField] private GameObject sleepUI;
+    [SerializeField] private Slider sleepSlider;
+    [SerializeField] private Text sleepNumber;
+
+    [SerializeField] private float hourlyRegen;
+    //DISABLE MANAGER
+
+
+
+}
